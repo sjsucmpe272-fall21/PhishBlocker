dc8e9055105e3e95745941ec3242c30796f2ac9b
adarshsam2896 <90799950+adarshsam2896@users.noreply.github.com>
2021-11-25 12:24:04 -0800

    Add files via upload
    Latest flask code with https and CROS enabled

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -1,9 +1,24 @@
-from flask import Flask,request
+import flask
+from flask import Flask, request,make_response
+
+from flask_cors import CORS, cross_origin
+
+#import ssl
+#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+#context.use_privatekey_file('key.pem')
+#context.use_certificate_file('cert.pem')
+
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('cert.pem', 'key.pem')
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/', methods=['POST','GET'])
+@cross_origin()
 def hello_world():  # put application's code here
     if(request.method=='POST'):
         s = request.get_json()
@@ -16,13 +31,23 @@ def hello_world():  # put application's code here
             for j in s[i]:
                 temp.append(j)
             url[i]=temp
-
-        return url
+        resp=flask.make_response(url)
+        header = resp.headers
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
 
     if(request.method=='GET'):
-        return "Here by a GET request"
+        #resp = flask.Response("Here by a GET request")
+        #header = resp.headers
+        #resp.headers['Access-Control-Allow-Origin'] = '*'
+        #resp.headers['X-Content-Type-Options'] = 'nosniff'
+        return "HERE!!!"
+
+
+
 
 
 
 if __name__ == '__main__':
-    app.run()
+    #app.run(ssl_context=context)
+    app.run(ssl_context=("cert.pem", "key.pem"))
