1e97890c992f4687fbb6f0f8ab8619479f2d9774
komaljoshi-sjsu <78235778+komaljoshi-sjsu@users.noreply.github.com>
2021-11-29 13:21:37 -0800

    Highlighting malicious URLs

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -7,16 +7,47 @@ window.onload = function() {
 		//urls[url].style.backgroundColor = "#FDFF47";
 	}
 
-	fetch("http://3.22.236.108/", {
+	//console.log('Sending input:',urlList)
+
+	fetch("https://ec2-18-189-22-50.us-east-2.compute.amazonaws.com:8000/", {
 		method: "POST",
-		mode: "cors",
 		referrer:"unsafe-url",
 		headers: {'Content-Type': 'application/json'} ,
-		body: {data:urlList}
+		body: JSON.stringify({data:urlList})
 	}).then(res => {
-		console.log('result is:',res);
+		res.text().then(function(text) {
+			console.log('result is:',text);
+			text = JSON.parse(text);
+			let urlResp = text.data;
+			for (url in urls) {
+				if(urlResp.includes(urls[url].href+""))
+					urls[url].style.backgroundColor = "#FDFF47";
+			}
+			// for (url in urlResp) {
+			// 	if(urlResp.includes(urls[url].href+"")) {
+			// 		console.log('includes url:',url);
+			// 		urls[url].style.backgroundColor = "#FDFF47";
+			// 	}
+
+					
+			// 	//urls[url].style.backgroundColor = "#FDFF47";
+			// }
+		  });
+		
 	}).catch(err=> {
 		console.log('error is:',err)
 	});
 		//highlightText(document.body,"create");
-}
+	}
+
+// function highlightText(element,text) {
+// 	var nodes = element.childNodes;
+// 	for (var i = 0, l = nodes.length; i < l; i++) {
+// 	  if (nodes[i].nodeType === 3) { // Node Type 3 is a text node
+// 		nodes[i].innerHTML = "<span style='background-color:red'>" + text + "</span>";
+// 	  }
+// 	  else if (nodes[i].childNodes.length > 0) {
+// 		highlightText(nodes[i],text);  // Not a text node or leaf, so check it's children
+// 	  }
+// 	}
+// }
